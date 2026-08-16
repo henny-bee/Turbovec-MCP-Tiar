1fad59801750f49059515b24105b7471ee53cfdb
henny <badaksaya09@gmail.com>
2026-08-16 14:25:03 +0200

    define general EmbeddingProvider protocol for modular embedding backends

---
+++ embeddings/base.py
@@ -0,0 +1,11 @@
+from typing import List, Protocol
+
+class EmbeddingProvider(Protocol):
+    @property
+    def dimension(self) -> int:
+        """Returns the dimension of generated embeddings."""
+        ...
+
+    def embed(self, texts: List[str]) -> List[List[float]]:
+        """Generates embeddings for a list of texts."""
+        ...
